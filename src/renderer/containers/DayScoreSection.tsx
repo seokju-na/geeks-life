de2c3e990dd134c596e7b6d7ddc07bf0839fe6a7
seokju-na <seokju.me@gmail.com>
2020-08-02 00:33:33 +0900

    Replace <ScoreMenuButton /> and use <NativeSelect /> component

--- src/renderer/containers/DayScoreSection.tsx
+++ src/renderer/containers/DayScoreSection.tsx
@@ -1,10 +1,20 @@
 import React, { HTMLProps, useCallback, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { createEnumKeyFind } from '../../core';
 import { DailyScore } from '../../core/domain';
+import { dailyScoreColorMap } from '../colors/daily-score-colors';
 import { selectForeground, styled } from '../colors/theming';
-import ScoreMenuButton from '../components/ScoreMenuButton';
+import NativeSelect from '../components/NativeSelect';
 import { actions } from '../store/actions';
 import { selectors } from '../store/selectors';
+
+const scores = [DailyScore.Low, DailyScore.Medium, DailyScore.High, DailyScore.Excellent];
+const getScoreKey = createEnumKeyFind(DailyScore);
+
+const scoreOptions = scores.map((score) => ({
+  value: score,
+  displayValue: getScoreKey(score),
+}));
 
 export default function DayScoreSection(props: HTMLProps<HTMLDivElement>) {
   const isDateToday = useSelector(selectors.isDateToday);
@@ -29,10 +39,22 @@ export default function DayScoreSection(props: HTMLProps<HTMLDivElement>) {
   return (
     <Section {...props}>
       <Title>{title}</Title>
-      <ScoreMenuButton
-        score={dailyLife?.score ?? DailyScore.None}
-        onScoreChange={handleDailyLifeScoreChange}
-      />
+      <NativeSelect<DailyScore>
+        value={dailyLife?.score ?? DailyScore.None}
+        options={scoreOptions}
+        onChange={handleDailyLifeScoreChange}
+      >
+        {(value) =>
+          value != null ? (
+            <>
+              {value !== DailyScore.None ? (
+                <ColorBox backgroundColor={dailyScoreColorMap[value]} />
+              ) : null}
+              {value === DailyScore.None ? `Not Selected` : getScoreKey(value)}
+            </>
+          ) : null
+        }
+      </NativeSelect>
     </Section>
   );
 }
@@ -49,4 +71,12 @@ const Title = styled.h2`
   font-size: 0.875rem;
   font-weight: 500;
   color: ${selectForeground('text')};
+`;
+
+const ColorBox = styled.div<{ backgroundColor?: string }>`
+  width: 1.5em;
+  height: 1em;
+  background-color: ${(p) => p.backgroundColor};
+  margin-right: 6px;
+  border-radius: 0.15em;
 `;
