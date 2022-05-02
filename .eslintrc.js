ff6edab85cb9d559b854849db57c6bc63d70b440
Seokju Na <seokju.me@toss.im>
2022-05-02 20:16:41 +0900

    initialize project
    using tauri, vite

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'eslint-config-prettier'],
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'react', 'react-hooks'],
+  settings: { 'import/resolver': { typescript: {} } },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-implicit-coercion': 'error',
+    'no-undef': 'off',
+    semi: 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    'no-warning-comments': [
+      'warn',
+      {
+        terms: ['TODO', 'FIXME', 'XXX', 'BUG'],
+        location: 'anywhere',
+      },
+    ],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    curly: ['error', 'all'],
+    eqeqeq: ['error', 'always', { null: 'ignore' }],
+    'import/no-duplicates': 'error',
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': [
+      'error',
+      {
+        additionalHooks: 'useRecoilCallback',
+      },
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.spec.ts', '**/*.spec.tsx', '**/*.test.ts', '**/*.test.tsx'],
+      rules: {
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
