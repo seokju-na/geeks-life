5db84a7dd4d0702dbfe614cad04a8169ca8ec148
Seokju Na <seokju.me@toss.im>
2022-05-28 23:23:55 +0900

    add `load_aggregate`

--- src-tauri/src/application/mod.rs
+++ src-tauri/src/application/mod.rs
@@ -1,5 +1,7 @@
 pub use crate::application::daily_life_snapshot::*;
+pub use crate::application::load_aggregate::*;
 pub use crate::application::snapshot_metadata::*;
 
 mod daily_life_snapshot;
+mod load_aggregate;
 mod snapshot_metadata;
