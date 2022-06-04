246e317f381bc70ae72d30e3dcd81a82cca460bb
Seokju Na <seokju.me@toss.im>
2022-06-04 17:39:25 +0900

    no path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src'),
-    },
-  },
   plugins: [react()],
 });
