f13046f6f7ea5e3edd0362c10e21e8094577c153
Seokju Na <seokju.me@toss.im>
2022-06-04 17:22:57 +0900

    macos titlebar patches

---
+++ src-tauri/src/patches/macos_titlebar.rs
@@ -0,0 +1,46 @@
+use cocoa::appkit::{NSWindow, NSWindowStyleMask, NSWindowTitleVisibility};
+use tauri::{Runtime, Window};
+
+// sources from https://github.com/tauri-apps/tauri/issues/2663#issuecomment-1123696703
+
+pub trait MacOSTitlebar {
+  #[cfg(target_os = "macos")]
+  fn set_transparent_titlebar(&self, title_transparent: bool, remove_toolbar: bool);
+}
+
+impl<R: Runtime> MacOSTitlebar for Window<R> {
+  #[cfg(target_os = "macos")]
+  fn set_transparent_titlebar(&self, title_transparent: bool, remove_tool_bar: bool) {
+    unsafe {
+      let id = self.ns_window().unwrap() as cocoa::base::id;
+      NSWindow::setTitlebarAppearsTransparent_(id, cocoa::base::YES);
+      let mut style_mask = id.styleMask();
+      style_mask.set(
+        NSWindowStyleMask::NSFullSizeContentViewWindowMask,
+        title_transparent,
+      );
+
+      if remove_tool_bar {
+        style_mask.remove(
+          NSWindowStyleMask::NSClosableWindowMask
+            | NSWindowStyleMask::NSMiniaturizableWindowMask
+            | NSWindowStyleMask::NSResizableWindowMask,
+        );
+      }
+
+      id.setStyleMask_(style_mask);
+
+      id.setTitleVisibility_(if title_transparent {
+        NSWindowTitleVisibility::NSWindowTitleHidden
+      } else {
+        NSWindowTitleVisibility::NSWindowTitleVisible
+      });
+
+      id.setTitlebarAppearsTransparent_(if title_transparent {
+        cocoa::base::YES
+      } else {
+        cocoa::base::NO
+      });
+    }
+  }
+}
---
+++ src-tauri/src/patches/mod.rs
@@ -0,0 +1,3 @@
+pub use crate::patches::macos_titlebar::*;
+
+mod macos_titlebar;
