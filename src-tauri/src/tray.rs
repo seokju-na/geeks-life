01dcfcfe49e6412a92ac655f2216961c6934238c
Seokju Na <seokju.me@toss.im>
2022-06-05 16:45:23 +0900

    change tray behavior

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,31 +1,18 @@
-use tauri::{
-  AppHandle, CustomMenuItem, Manager, Runtime, SystemTray, SystemTrayEvent, SystemTrayMenu,
-};
+use tauri::{AppHandle, Manager, Runtime, SystemTray, SystemTrayEvent};
+
+use crate::window::{WindowExtra, MAIN_WIN};
 
 pub fn tray() -> SystemTray {
-  let open = CustomMenuItem::new("open".to_string(), "Open").accelerator("CmdOrCtrl+O");
-  let quit = CustomMenuItem::new("quit".to_string(), "Quit").accelerator("CmdOrCtrl+Q");
-  let menu = SystemTrayMenu::new().add_item(open).add_item(quit);
-
-  SystemTray::new().with_menu(menu)
+  SystemTray::new()
 }
 
 pub fn handle_tray<R>(app: &AppHandle<R>, event: SystemTrayEvent)
 where
   R: Runtime,
 {
-  if let SystemTrayEvent::MenuItemClick { id, .. } = event {
-    match id.as_str() {
-      "open" => {
-        if let Some(win) = app.get_window("main") {
-          win.show().unwrap();
-          win.set_focus().unwrap();
-        }
-      }
-      "quit" => {
-        std::process::exit(0);
-      }
-      _ => {}
+  if let SystemTrayEvent::LeftClick { .. } = event {
+    if let Some(win) = app.get_window(MAIN_WIN) {
+      win.toggle().unwrap();
     }
   }
 }
