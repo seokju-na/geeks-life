7801d36d13a3698308e4e7f89ded697bf009ecfa
seokju-na <seokju.me@toss.im>
2022-06-05 17:05:26 +0900

    add `<ToggleGroup />`, `<ToggleGroupItem />` components (#49)
    * remove tsc
    
    * add colors
    
    * update theme colors
    
    * change background color
    
    * remove radix colors
    
    * add `<ToggleGroup />` component

---
+++ src/components/ToggleGroup/index.tsx
@@ -0,0 +1,28 @@
+import * as Primitives from '@radix-ui/react-toggle-group';
+import { styled } from '../../styles';
+
+export const ToggleGroup = styled(Primitives.Root, {
+  display: 'inline-flex',
+  borderRadius: '$normal',
+  border: `1px solid $divider`,
+});
+
+export const ToggleGroupItem = styled(Primitives.Item, {
+  all: 'unset',
+  backgroundColor: '$raisedButton',
+  color: '$text',
+  height: 28,
+  display: 'flex',
+  fontSize: '$sm',
+  padding: '0 $md',
+  lineHeight: 1,
+  alignItems: 'center',
+  justifyContent: 'center',
+  transition: 'background-color 0.15s ease-in-out 0s, box-shadow 0.2s ease',
+  '&:first-child': { borderTopLeftRadius: '$normal', borderBottomLeftRadius: '$normal' },
+  '&:last-child': { borderTopRightRadius: '$normal', borderBottomRightRadius: '$normal' },
+  '& + &': { borderLeft: '1px solid $divider' },
+  '&[data-state=on]': { backgroundColor: '$selectedButton' },
+  $$shadowColor: '$colors$focusRing',
+  '&:focus': { position: 'relative', boxShadow: '0 0 0 2px $$shadowColor' },
+});
