c576c97a6a64a4edab43183777a1b2f45680d2b6
Seokju Na <seokju.me@toss.im>
2022-06-10 22:19:02 +0900

    query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
+import { QueryClientProvider } from 'react-query';
 import App from './App';
 import './global.css';
+import { queryClient } from './queryClient';
 import { injectTheme } from './styles';
 
 injectTheme();
 
 render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
   document.getElementById('root')
 );
