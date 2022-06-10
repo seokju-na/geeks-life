3ae69941f7845c348787b9770e81140b96eda779
Seokju Na <seokju.me@toss.im>
2022-06-10 22:18:57 +0900

    add crypto polyfill

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,18 @@
+import { clearMocks } from '@tauri-apps/api/mocks';
 import '@testing-library/jest-dom';
+import { randomFillSync } from 'crypto';
+import { afterEach, beforeAll } from 'vitest';
+
+// required for tauri ipc.
+// see: https://tauri.studio/v1/guides/testing/mocking
+beforeAll(() => {
+  (window as any).crypto = {
+    getRandomValues: (buffer: Buffer) => {
+      return randomFillSync(buffer);
+    },
+  };
+});
+
+afterEach(() => {
+  clearMocks();
+});
