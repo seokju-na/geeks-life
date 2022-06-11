972534ca4e39d17e981ba03d16f48c6393d2cad6
Seokju Na <seokju.me@gmail.com>
2022-06-11 22:10:49 +0900

    add `DailyLifeView` model

---
+++ src/models/DailyLifeView.ts
@@ -0,0 +1,1 @@
+export type DailyLifeView = 'week' | 'month';
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,5 @@
 export * from './DailyLife';
 export * from './DailyLog';
+export * from './DailyLifeCommand';
+export * from './DailyLifeView';
 export * from './Score';
