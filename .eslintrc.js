7507cf6460d9cbb853b445838dfec30c3dbf36ae
Seokju Na <seokju.me@gmail.com>
2022-06-11 22:10:23 +0900

    ignore some rules on test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,11 +48,21 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['**/*.js', '**/*.spec.ts', '**/*.spec.tsx', '**/*.test.ts', '**/*.test.tsx'],
+      files: [
+        '**/*.js',
+        '**/*.spec.ts',
+        '**/*.spec.tsx',
+        '**/*.test.ts',
+        '**/*.test.tsx',
+        'src/testing/**/*.ts',
+        'src/testing/**/*.tsx',
+      ],
       rules: {
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/no-inferrable-types': 'off',
         '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
       },
     },
   ],
