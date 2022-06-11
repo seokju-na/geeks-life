395d53c9ebc2e89a5c6d2e5a9b23f5e034855bf9
Seokju Na <seokju.me@toss.im>
2022-06-11 13:45:55 +0900

    add `range` util

---
+++ src/utils/range.ts
@@ -0,0 +1,12 @@
+export function range(start: number, end?: number, step = 1): number[] {
+  const _start = end === undefined ? 0 : start;
+  const _end = end === undefined ? start : end;
+
+  const output = [];
+  let current = _start;
+  while (current < _end) {
+    output.push(current);
+    current += step;
+  }
+  return output;
+}
