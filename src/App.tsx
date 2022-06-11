4374f06fe8efd805df397b17c168908a9dbd0113
Seokju Na <seokju.me@toss.im>
2022-06-11 14:54:37 +0900

    add `ESC` key handler
    When `ESC` key event occurred:
    - hide main window when focus lost.
    - otherwise blur the active element.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
-import { ToggleGroup, ToggleGroupItem } from './components/ToggleGroup';
+import { emit } from '@tauri-apps/api/event';
+import { useSubscription } from 'observable-hooks';
+import { useState } from 'react';
+import { fromEvent, filter } from 'rxjs';
+import { ButtonToggle, ButtonToggleItem, DailyLifeCalendar } from './components';
 
 export default function App() {
+  useEscKeydown();
+
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   return (
     <main>
       <div style={{ padding: 24 }}>
-        <ToggleGroup type="single">
-          <ToggleGroupItem value="weekly">Weekly</ToggleGroupItem>
-          <ToggleGroupItem value="monthly">Monthly</ToggleGroupItem>
-        </ToggleGroup>
+        <ButtonToggle type="single">
+          <ButtonToggleItem value="weekly">Weekly</ButtonToggleItem>
+          <ButtonToggleItem value="monthly">Monthly</ButtonToggleItem>
+        </ButtonToggle>
       </div>
       <div style={{ padding: 24 }}>
-        <button>test</button>
+        <DailyLifeCalendar view="week" selectedDate={selectedDate} onSelectDate={setSelectedDate} />
       </div>
     </main>
   );
+}
+
+const keydown$ = fromEvent<KeyboardEvent>(document, 'keydown');
+
+function useEscKeydown() {
+  useSubscription(keydown$.pipe(filter(e => !e.defaultPrevented && e.key === 'Escape')), event => {
+    const { activeElement } = document;
+
+    // Hide window when focus lost.
+    if (activeElement === null || activeElement === document.body) {
+      event.preventDefault();
+      emit('hide');
+    } else {
+      (activeElement as HTMLElement)?.blur();
+    }
+  });
 }
