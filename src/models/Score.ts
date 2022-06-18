26d344c09af03a96136ef9be7542dda2edfa1e13
Seokju Na <seokju.me@gmail.com>
2022-06-18 15:41:10 +0900

    scheming `Score` with zod

--- src/models/Score.ts
+++ src/models/Score.ts
@@ -1,1 +1,22 @@
-export type Score = 'low' | 'medium' | 'high' | 'excellent';
+import * as z from 'zod';
+
+export const Score = z.nativeEnum({
+  Low: 'low',
+  Medium: 'medium',
+  High: 'high',
+  Excellent: 'excellent',
+} as const);
+export type Score = z.infer<typeof Score>;
+
+export function getScoreColor(score: Score): string {
+  switch (score) {
+    case 'low':
+      return '#9be9a8';
+    case 'medium':
+      return '#40c463';
+    case 'high':
+      return '#30a14e';
+    case 'excellent':
+      return '#216e39';
+  }
+}
