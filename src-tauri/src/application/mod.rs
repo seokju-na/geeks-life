bf9e5f945981865f6d640363528a5942de7e17fd
Seokju Na <seokju.me@gmail.com>
2022-06-19 20:28:06 +0900

    rename `queries.rs` -> `query_handler.rs`

--- src-tauri/src/application/mod.rs
+++ src-tauri/src/application/mod.rs
@@ -1,11 +1,11 @@
 pub use crate::application::application::*;
 pub use crate::application::command_handler::*;
 pub use crate::application::daily_life_snapshot::*;
-pub use crate::application::queries::*;
+pub use crate::application::query_handler::*;
 pub use crate::application::snapshot_metadata::*;
 
 mod application;
 mod command_handler;
 mod daily_life_snapshot;
-mod queries;
+mod query_handler;
 mod snapshot_metadata;
