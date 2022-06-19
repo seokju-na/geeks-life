e50ce65f23a86c0948affb77e1f335e68cb4c8dd
Seokju Na <seokju.me@gmail.com>
2022-06-19 19:07:56 +0900

    fetch github emojis

---
+++ scripts/fetch_emoji_resources.mjs
@@ -0,0 +1,53 @@
+import { promisify } from 'node:util';
+import stream from 'node:stream';
+import path from 'node:path';
+import fs from 'node:fs';
+import { fileURLToPath } from 'url';
+import got from 'got';
+
+const pipeline = promisify(stream.pipeline);
+const mkdir = promisify(fs.mkdir);
+
+const availableExtensions = ['.png', '.svg', '.jpg', '.jpeg'];
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const resourcesDir = path.resolve(__dirname, '../src-tauri/resources/emoji/github');
+
+try {
+  await mkdir(resourcesDir);
+} catch {
+  //
+}
+
+/** @type {{ [id: string]: string }} */
+const response = await got.get('https://api.github.com/emojis', {
+  headers: {
+    'User-Agent': 'https://github.com/seokju-na/geeks-life',
+  },
+}).json();
+
+const data = {};
+const githubEmojis = Object.entries(response);
+let i = 1;
+
+for (const [name, url] of githubEmojis) {
+  const ext = availableExtensions.find(
+    x => url.toLowerCase().includes(x),
+  );
+  const file = path.join(resourcesDir, `${name}${ext}`);
+
+  await pipeline(
+    got.stream(url),
+    fs.createWriteStream(file),
+  );
+  data[name] = {
+    local_path: `emoji/github/${name}${ext}`,
+  };
+  console.log(`(${i}/${githubEmojis.length}) ${file}`);
+  i += 1;
+}
+
+await fs.writeFile(
+  path.join(resourcesDir, 'data.json'),
+  JSON.stringify(data),
+  'utf8',
+);
