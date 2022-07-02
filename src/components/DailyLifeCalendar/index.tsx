5ba6e6bd6e821d8eb00acb8e01b61aab43b5c37a
Seokju Na <seokju.me@gmail.com>
2022-07-02 17:21:42 +0900

    support month view

--- src/components/DailyLifeCalendar/index.tsx
+++ src/components/DailyLifeCalendar/index.tsx
@@ -1,38 +1,56 @@
-import { addDays, subDays } from 'date-fns';
+import { addDays, subDays, startOfWeek, endOfWeek, startOfMonth, endOfMonth, isSameDay } from 'date-fns';
 import { KeyboardEvent, useEffect, useRef } from 'react';
-import { useSelectedDateState, useDailyLifeViewState } from '../../hooks';
+import { match } from 'ts-pattern';
+import { useSelectedDateState, useDailyLifeViewState, useDailyLifes } from '../../hooks';
+import { getDailyLifeId } from '../../models';
 import { styled } from '../../styles';
-import { Switch } from '../Switch';
-import { Week } from './Week';
+import { getDaysOfWeekInCalendar, getDaysOfMonthInCalendar } from '../../utils/calendar';
+import { Day } from './Day';
 
 export function DailyLifeCalendar() {
   const ref = useRef<HTMLDivElement>(null);
   const { value: selectedDate, setValue: setSelectedDate } = useSelectedDateState();
   const { value: view } = useDailyLifeViewState();
 
+  const params = match(view)
+    .with('week', () => ({
+      start: startOfWeek(selectedDate),
+      end: endOfWeek(selectedDate),
+    }))
+    .with('month', () => ({
+      start: startOfWeek(startOfMonth(selectedDate)),
+      end: endOfWeek(endOfMonth(selectedDate)),
+    }))
+    .run();
+  const { data: items = [] } = useDailyLifes(params);
+
   useEffect(() => {
     ref.current?.focus();
   }, []);
 
+  const dates = match(view)
+    .with('week', () => getDaysOfWeekInCalendar(selectedDate))
+    .with('month', () => getDaysOfMonthInCalendar(selectedDate))
+    .run();
+
   return (
     <Wrapper ref={ref} role="group" tabIndex={0} onKeyDown={useKeyboardHandler()}>
-      <Switch
-        value={view}
-        caseBy={{
-          week: <Week selectedDate={selectedDate} onSelectDate={setSelectedDate} />,
-          month: <Todo />,
-        }}
-      />
+      <Grid>
+        {dates.map(date => {
+          const id = getDailyLifeId(date);
+          const item = items.find(x => x.id === id);
+          const selected = isSameDay(date, selectedDate);
+          const handleClick = () => setSelectedDate(date);
+
+          return <Day key={id} item={item} selected={selected} onClick={handleClick} />;
+        })}
+      </Grid>
     </Wrapper>
   );
 }
 
-function Todo() {
-  return <>todo</>;
-}
-
 const Wrapper = styled('div', {
-  padding: '$sm $lg',
+  padding: '$md $lg',
   '&:focus': {
     border: 'none',
     outline: 0,
@@ -42,6 +60,12 @@ const Wrapper = styled('div', {
       boxShadow: '0 0 0 2px $$focusedColor',
     },
   },
+});
+
+const Grid = styled('div', {
+  display: 'grid',
+  gridGap: '6px',
+  gridTemplateColumns: 'repeat(7, minmax(0, 1fr))',
 });
 
 function useKeyboardHandler() {
