0ed7c53afe826511602ae9af1dda0acd022e1f6c
Seokju Na <seokju.me@gmail.com>
2022-07-02 17:21:35 +0900

    add `getDaysOfMonthInCalendar`, rename `getDaysOfWeekInCalendar`

--- src/utils/calendar.test.ts
+++ src/utils/calendar.test.ts
@@ -1,20 +1,36 @@
 import { test } from 'vitest';
-import { getWeek } from './calendar';
+import { getDaysOfWeekInCalendar, getDaysOfMonthInCalendar } from './calendar';
+import { range } from './range';
 
-test('getWeek', () => {
-  it('get 7 dates starts on monday', () => {
+test('getDaysOfWeekInCalendar', () => {
+  it('get 7 days of week which starts on sunday', () => {
     const june2ndWeek = [
-      new Date(2022, 5, 5, 0, 0, 0, 0),
-      new Date(2022, 5, 6, 0, 0, 0, 0),
-      new Date(2022, 5, 7, 0, 0, 0, 0),
-      new Date(2022, 5, 8, 0, 0, 0, 0),
-      new Date(2022, 5, 9, 0, 0, 0, 0),
-      new Date(2022, 5, 10, 0, 0, 0, 0),
-      new Date(2022, 5, 11, 0, 0, 0, 0),
+      new Date(2022, 5, 5),
+      new Date(2022, 5, 6),
+      new Date(2022, 5, 7),
+      new Date(2022, 5, 8),
+      new Date(2022, 5, 9),
+      new Date(2022, 5, 10),
+      new Date(2022, 5, 11),
     ] as const;
 
     for (const date of june2ndWeek) {
-      expect(getWeek(date)).toEqual(june2ndWeek);
+      expect(getDaysOfWeekInCalendar(date)).toEqual(june2ndWeek);
+    }
+  });
+});
+
+test('getDaysOfMonthInCalendar', () => {
+  it('get all days of month in calendar', () => {
+    const june = range(1, 31).map(x => new Date(2022, 6, x));
+    const juneInCalendar = [
+      ...range(26, 30).map(x => new Date(2022, 5, x)),
+      ...june,
+      ...range(1, 6).map(x => new Date(2022, 7, x)),
+    ] as const;
+
+    for (const date of june) {
+      expect(getDaysOfMonthInCalendar(date)).toEqual(juneInCalendar);
     }
   });
 });
--- src/utils/calendar.ts
+++ src/utils/calendar.ts
@@ -1,12 +1,18 @@
-import { addDays, startOfWeek, toDate } from 'date-fns';
+import { addDays, startOfWeek, toDate, startOfMonth, endOfWeek, endOfMonth, differenceInDays } from 'date-fns';
 import { range } from './range';
 
-type Week = [Date, Date, Date, Date, Date, Date, Date];
-
-export function getWeek(date: Date): Week {
+export function getDaysOfWeekInCalendar(date: Date): Date[] {
   const start = normalizeDate(startOfWeek(date));
 
-  return range(7).map(i => addDays(start, i)) as Week;
+  return range(7).map(i => addDays(start, i));
+}
+
+export function getDaysOfMonthInCalendar(date: Date): Date[] {
+  const start = normalizeDate(startOfWeek(startOfMonth(date)));
+  const end = normalizeDate(endOfWeek(endOfMonth(date)));
+  const diff = differenceInDays(end, start);
+
+  return range(diff).map(i => addDays(start, i));
 }
 
 function normalizeDate(date: Date): Date {
