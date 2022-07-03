5e899dc480fdfd39d6f4373ff2d88f8888ca90fa
Seokju Na <seokju.me@gmail.com>
2022-07-03 18:57:15 +0900

    add comments

--- src-tauri/src/windows.rs
+++ src-tauri/src/windows.rs
@@ -6,27 +6,31 @@ use crate::patches::TransparentTitlebar;
 
 const MAIN_WIN: &str = "main";
 
-pub fn setup_windows<R>(app: &mut App<R>)
-where
-  R: Runtime,
-{
-  let win = app.get_main_window();
+pub fn setup_windows<R: Runtime>(app: &mut App<R>) {
+  // [macOS] set main window transparent titlebar
   #[cfg(target_os = "macos")]
-  win.set_transparent_titlebar(true, true);
+  {
+    let win = app.get_main_window();
+    win.set_transparent_titlebar(true, true);
+  }
 
+  // register "hide" event for main window.
   let win = app.get_main_window();
   win.clone().listen("hide", move |_| {
     win.hide().unwrap();
   });
 
+  // register "geeks-life://resize" event for main window.
   let win = app.get_main_window();
   win.clone().listen("geeks-life://resize", move |event| {
     let size = from_str::<LogicalSize<f64>>(event.payload().unwrap()).unwrap();
     win.set_size(Size::Logical(size)).unwrap();
   });
 
+  // register window event.
   let win = app.get_main_window();
   win.clone().on_window_event(move |event| {
+    // hide window when looses focuses (production only).
     if let WindowEvent::Focused(focused) = event {
       if win.is_visible().unwrap() && !(*focused) {
         #[cfg(not(debug_assertions))]
